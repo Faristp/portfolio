9790b97f28865088dd3bceacdd1cdf127e25a302
Faristp <95973113+Faristp@users.noreply.github.com>
2026-06-19 14:20:25 +0000

    Initial portfolio setup

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+  return (
+    <section className="h-screen flex items-center">
+      <div>
+        <h1 className="text-7xl font-bold">
+          Faris Muhammed
+        </h1>
+
+        <p className="mt-5">
+          Building scalable applications.
+        </p>
+      </div>
+    </section>
+  );
+}
